8931e1a96779f920d8d0f2cf414a3faa545c94a2
MuhWldns <88471861+MuhWldns@users.noreply.github.com>
2026-05-21 22:49:59 +0700

    feat: add reviews table, Review model, and relations

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,4 +48,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(UserAddress::class);
     }
+
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
 }
